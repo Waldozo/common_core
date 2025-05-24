83e43ea1c6f2ee5c5b328a2da7254eb8672bdcb9
Walid Larbi-Aissa <wlarbi-a@f1r3s2.clusters.42paris.fr>
2025-05-24 15:05:10 +0200

    minishell

--- minishell/parsing/minishell.c
+++ minishell/parsing/minishell.c
@@ -6,36 +6,51 @@
 /*   By: wlarbi-a <wlarbi-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 16:23:24 by wlarbi-a          #+#    #+#             */
-/*   Updated: 2025/05/02 17:00:27 by wlarbi-a         ###   ########.fr       */
+/*   Updated: 2025/05/23 20:53:37 by wlarbi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    (void)argv;
-    char *input;
-    input = NULL;
+	t_struct	*data;
+	t_struct	*tmp;
 
-    if(argc != 1)
-    {
-        printf("Error : need only one arguments\n");
-        return(0);
-    }
-    else
-    {
-        while(1)
-        {
-            input = readline("💻🔞 minishell > ");
-            if(input == NULL)
-                break;
-            if (ft_strlen(input) > 0)
-            {
-                add_history(input);
-                free(input);
-            }
-        }
-    }
-    return(0);
+	(void)argv;
+	tmp = NULL;
+	data = NULL;
+	if (argc != 1)
+	{
+		printf("Error: need only one argument\n");
+		return (1);
+	}
+	data = (t_struct *)malloc(sizeof(t_struct));
+	if (!data)
+	{
+		perror("Error allocating memory");
+		return (1);
+	}
+	while (1)
+	{
+		data->str = readline("💻 minishell > ");
+		if (data->str == NULL)
+			break ;
+		if (ft_strlen(data->str) > 0)
+		{
+			add_history(data->str);
+			if (parsing(data))
+			{
+				tmp = data->next;
+				while (tmp)
+				{
+					printf("{%d -> %s}\n", tmp->type, tmp->str);
+					tmp = tmp->next;
+				}
+			}
+		}
+		free(data->str);
+	}
+	free(data);
+	return (0);
 }
--- minishell/parsing/utils3.c
+++ minishell/parsing/utils3.c
@@ -1,24 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.c                                            :+:      :+:    :+:   */
+/*   utils3.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wlarbi-a <wlarbi-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/02 16:53:18 by wlarbi-a          #+#    #+#             */
-/*   Updated: 2025/05/02 16:55:36 by wlarbi-a         ###   ########.fr       */
+/*   Created: 2025/05/23 16:14:10 by wlarbi-a          #+#    #+#             */
+/*   Updated: 2025/05/23 16:45:57 by wlarbi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int ft_strlen(char *str)
+char	*ft_strjoin(char *s1, char *s2)
 {
-    int i;
-    i = 0;
-    while(str[i])
-    {
-        i++;
-    }
-    return(i);
+	size_t	i;
+	size_t	j;
+	char	*dest;
+
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	dest = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!dest)
+		return (NULL);
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		dest[i + j] = s2[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
 }
